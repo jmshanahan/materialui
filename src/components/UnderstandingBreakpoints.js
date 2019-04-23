5a632429a7f0a185bb7786722c170aea151247f1
jmshanahan <comeraghsolutions@gmail.com>
2019-04-23 11:32:37 +0100

    Initial set up and grid system

---
+++ src/components/UnderstandingBreakpoints.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1
+  },
+  paper: {
+    padding: theme.spacing.unit * 4,
+    textAlign: 'left',
+    color: theme.palette.text.secondary
+    // square: true
+  }
+});
+const Container = props => <Grid container {...props} />;
+const Item = props => <Grid item {...props} />;
+
+const UnderstandingBreakpoints = withStyles(styles)(({ classes }) => (
+  <div className={classes.root}>
+    <Container spacing={4}>
+      <Item xs={12} sm={6} md={3}>
+        <Paper className={classes.paper}>xs=12 sm=6 md=3</Paper>
+      </Item>
+      <Item xs={12} sm={6} md={3}>
+        <Paper className={classes.paper}>xs=12 sm=6 md=3</Paper>
+      </Item>
+      <Item xs={12} sm={6} md={3}>
+        <Paper className={classes.paper}>xs=12 sm=6 md=3</Paper>
+      </Item>
+      <Item xs={12} sm={6} md={3}>
+        <Paper className={classes.paper}>xs=12 sm=6 md=3</Paper>
+      </Item>
+    </Container>
+  </div>
+));
+
+export default UnderstandingBreakpoints;
